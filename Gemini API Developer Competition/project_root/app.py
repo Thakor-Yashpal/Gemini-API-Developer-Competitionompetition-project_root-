ebdd99a4feb839269450961a59eaf1dc0ac09cc5
Th-Yash <thakoryashpal755@gmail.com>
2024-06-22 07:44:31 +0530

    Update app.py

--- Gemini API Developer Competition/project_root/app.py
+++ Gemini API Developer Competition/project_root/app.py
@@ -1,15 +1,13 @@
-# app.py
+
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route for the recommendations page
 @app.route('/recommendations')
 def recommendations():
     return render_template('recommendations.html')
